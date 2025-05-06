763f164095a1e73dd7240cd11f398beda9f0b191
KRISHNA-12082006 <kkrocks2022@gmail.com>
2025-05-06 14:34:02 +0530

    Backend databbase established

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,23 @@
 from flask import request, jsonify
 from config import app, db
-
-groceries = ()
+from models import Grocery
 
 @app.route('/')
 def home():
     return "Your Flask backend is working!"
 
-
-@app.route("/groceries", methods = ["GET"])
+@app.route("/groceries", methods=["GET"])
 def get_groceries():
-    return jsonify(groceries)
+    groceries = Grocery.query.all()
+    return jsonify([g.to_dict() for g in groceries])
 
-@app.route("/groceries", methods = ["POST"])
+@app.route("/groceries", methods=["POST"])
 def add_grocery():
     data = request.get_json()
-    groceries.append(data)
-    return jsonify[{"message": "Grocery added", "data": data}], 201
+    new_grocery = Grocery(name=data["name"])
+    db.session.add(new_grocery)
+    db.session.commit()
+    return jsonify({"message": "Grocery added", "data": new_grocery.to_dict()}), 201
 
 
 if __name__ == "__main__":
